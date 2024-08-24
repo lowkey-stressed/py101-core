e97acd2063c3ff920dd66d4f31338f14912a1420
Stressed Lowkey <storyfromtheast@icloud.com>
2024-08-23 20:40:58 -0700

    Calculator.py V2 + Calculator_messages.json

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -4,53 +4,75 @@
 # Perform the operation on the two numbers
 # Print result to terminal
 
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 def prompt(message):
     print(f'==>{message}')
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     return False
 
-prompt('Welcome to Calculator!')
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
 
-# Ask user for first number
-prompt('Please enter your first number:')
-number1 = input()
 
-while invalid_number(number1):
-    prompt("Hmm...that doesn't look like a valid number. Please try again.")
+prompt(messages('welcome'))
+
+while True: # Outer While loop to allow for multiple calculations
+    # Ask user for first number
+    prompt('Please enter your first number:')
     number1 = input()
 
+    while invalid_number(number1):
+        prompt(messages('invalid_number','en'))
+        number1 = input()
 
-prompt('Please enter your second number:')
-number2 = input()
 
-while invalid_number(number2):
-    prompt("Hmm...that doesn't look like a valid number. Please try again.")
+    prompt('Please enter your second number:')
     number2 = input()
 
-prompt("""What operation would you like to perform?\n'
-1) Add 2) Subtract 3) Multiply 4) Divide""")
-operation = input()
+    while invalid_number(number2):
+        prompt(messages('invalid_number','es'))
+        number2 = input()
 
-while operation not in ['1','2','3','4']:
-    # ask again
-    prompt('You must choose 1, 2, 3, or 4')
+    prompt("""What operation would you like to perform?\n
+    1) Add 2) Subtract 3) Multiply 4) Divide""")
     operation = input()
 
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) * int(number2)
-    case  '4':
-        output = int(number1) // int(number2)
+    while operation not in ['1','2','3','4']:
+        # ask again
+        prompt('You must choose 1, 2, 3, or 4')
+        operation = input()
+
+    match operation:
+        case '1':
+            output = float(number1) + float(number2)
+        case '2':
+            output = float(number1) - float(number2)
+        case '3':
+            output = float(number1) * float(number2)
+        case  '4':
+            output = float(number1) // float(number2)
 
 
 
-prompt(f"The result is: {output}")
+    prompt(f'The result is: {output}')
+    prompt("Do you want to another calculation? Enter 'Yes' or 'Y'")
+    answer = input()
+
+    if answer and answer[0].lower() != 'y':
+        prompt('Thank you for using our calculator!')
+        break
+    
+    
+   
+
+        
+   
